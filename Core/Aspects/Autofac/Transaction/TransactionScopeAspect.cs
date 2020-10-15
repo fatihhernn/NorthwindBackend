9a71ffdab1f7ae16d69c72a87083d364ba92e195
fatih eren <fatihhernn@gmail.com>
2020-10-15 14:48:31 +0300

    Transaction Scope Aspect Yazılması core->aspects->autofac->transaction->TransactionScopeAspect.cs

---
+++ Core/Aspects/Autofac/Transaction/TransactionScopeAspect.cs
@@ -0,0 +1,29 @@
+﻿using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace Core.Aspects.Autofac.Transaction
+{
+    public class TransactionScopeAspect : MethodInterception //burada Intercept metodunu override edelim
+    {
+        public override void Intercept(IInvocation invocation)
+        {
+            using (TransactionScope transactionScope=new TransactionScope())//transaction yazmak için TRANSACTIONSCOPE devreye sokmamız gerekiyor . disposable pattern yapısına uygun olması için using bloğuna alırız
+            {
+                try
+                {
+                    invocation.Proceed();
+                    transactionScope.Complete();
+                }
+                catch (Exception)
+                {
+                    transactionScope.Dispose();
+                    throw;
+                }
+            }
+        }
+    }
+}
