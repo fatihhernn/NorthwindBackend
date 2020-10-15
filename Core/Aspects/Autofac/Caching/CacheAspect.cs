a90917ac55c3e323d9b4bf85dae4df42c92d5bc0
fatih eren <fatihhernn@gmail.com>
2020-10-15 18:34:56 +0300

    cacheInterceptionAspect yazıldı

---
+++ Core/Aspects/Autofac/Caching/CacheAspect.cs
@@ -0,0 +1,37 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Caching;
+using Core.IoC;
+using Core.Utilities.Interceptors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace Core.Aspects.Autofac.Caching
+{
+     public class CacheAspect:MethodInterception
+    {
+        private int _duration;//kullanıcıdan istiyor alcağız
+        private ICacheManager _cacheManager;//memorycache manager mı, redix mi ? hangisi olduğunu burada vercez
+
+        public CacheAspect(int duration=60)//default olarak 60 yazalım. istenirse girilebilir.
+        {
+            _duration = duration;
+            _cacheManager = ServiceTool.ServiceProvider.GetService<ICacheManager>();//ICacheManager :  memory cache gelecek buraya
+        }
+        public override void Intercept(IInvocation invocation)//key değeri olarak metod ismi ve parametre şeklinde olacak .. product.GetByCategory(1,sfsdas)
+        {
+            var metotName = string.Format($"{invocation.Method.ReflectedType.FullName}.{invocation.Method.Name}");//product.getall
+            var arguments = invocation.Arguments.ToList();
+            var key = $"{metotName}({string.Join(",",arguments.Select(x=>x?.ToString()??"<Null>"))})";//operasyonun içeriği bazlı bir caching işlemi gerçekleştirmeye çalışıyoruz
+            if (_cacheManager.IsAdd(key))//bu key daha önce eklenmiş ise , metodu hiç çalıştırma o metoda ait return value döndür
+            {
+                invocation.ReturnValue = _cacheManager.Get(key);
+                return;
+            }
+            invocation.Proceed();
+            _cacheManager.Add(key, invocation, _duration);
+        }
+    }
+}
--- Core/DependencyCore/CoreModule.cs
+++ Core/DependencyCore/CoreModule.cs
@@ -1,4 +1,7 @@
-﻿using Core.IoC;
+﻿using Core.CrossCuttingConcerns.Caching;
+using Core.CrossCuttingConcerns.Caching.Microsoft;
+using Core.IoC;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -13,6 +16,11 @@ namespace Core.DependencyCore
             services.AddMemoryCache();//normalde bunu startup da eklemiştik ama bunu burada merkezileştirdik
 
             //burada bir extention yazacağız, IServiceCollection u extend edecek
+
+            //sisteme ekle ilk olarak
+            //birisi senden ICacheManager isterse,ona MemoryCacheManager ver - redix e geçilirse değiştirilip redisx yapılır.
+            services.AddSingleton<ICacheManager, MemoryCacheManager>();
+
         }
     }
 }
